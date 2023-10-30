b5cfea70402bce48341ed2fe483235b907ed699d
yyh <woyunkr@gmail.com>
2023-10-30 21:59:15 +0900

    feat(baseball): create skeleton code

---
+++ src/main/java/baseball/domain/Judgment.java
@@ -0,0 +1,13 @@
+package baseball.domain;
+
+import java.util.List;
+
+public class Judgment {
+    public int collectCount(List<Integer> computer, List<Integer> player) {
+        return 0;
+    }
+
+    public boolean hasPlace(int placeNumber, int number) {
+        return true;
+    }
+}
